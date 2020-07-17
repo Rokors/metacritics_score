b4a459bcf166be4bb06dba325d6a01f6b96bb951
Rokors <rokorsin@gmail.com>
2020-07-17 21:56:56 +0300

    still avg results, useless code removed

--- score_regression_v0.3.py
+++ score_regression_v0.3.py
@@ -14,45 +14,29 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
 from tensorflow.keras import layers
+import re
 
 def build_model():
   model = keras.Sequential([
-    layers.Dense(512, activation='softmax', input_shape=[len(X[0])]),
-    layers.Dense(128, activation='elu'),
-    layers.Dense(64, activation='relu'),
+    layers.Dense(16, activation='softmax', input_shape=[len(X[0])]),
+    layers.Dense(8, activation='relu'),
+    layers.Dense(4, activation='relu'),
     layers.Dense(2)
   ])
 
   #optimizer = tf.keras.optimizers.RMSprop(0.01,centered=True)
-  optimizer = tf.keras.optimizers.Adam(0.001)
+  optimizer = tf.keras.optimizers.Adam(0.01)
   model.compile(loss='mse',
                 optimizer=optimizer,
                 metrics=['mae', 'mse'])
   return model
-
-def differ(row,vocab,numset):
-#    if num == 1:
-    for i in range(len(vocab)):
-        if row.Platform == vocab[i]:
-            return numset[i]
-#    if num == 2:
-#        for i in range(len(vocab_platform)):
-#            if row.Platform == vocab_platform[i]:
-#                return numset_platform[i]        
-def lowcase_clean(row):
-    testing = row.alltext.lower()
-    row.alltext = row.alltext.lower()
-    return row.alltext.lower()
-#    if num == 2:
-#        for i in range(len(vocab_platform)):
-#            if row.Platform == vocab_platform[i]:
-#                return numset_platform[i]        
+       
 
 # load dataset
 pbl = pd.read_table('complete_base.csv')
 pbl.dropna(subset = ['UserReviews', 'CriticReviews', 'MetaScore', 'UserScore','Publisher', 'Developer'], inplace=True)
 pbl = pbl.drop(pbl[pbl.CriticReviews < 5].index)
-pbl = pbl.drop(pbl[pbl.UserReviews < 10].index)
+pbl = pbl.drop(pbl[pbl.UserReviews < 50].index)
 
 
 
@@ -65,6 +49,12 @@ pbl['alltext'] = pbl.Title +pbl.space+ pbl.Platform +pbl.space+ pbl.Publisher +p
 #clean dataset
 pbl['alltext'] = pbl['alltext'].str.lower()
 #pass
+texts = pbl['alltext']
+texts = texts.tolist()
+shortword = re.compile(r'\W*\b\w{1,3}\b')
+for i in range(len(texts)):
+    texts[i] = shortword.sub('',texts[i])
+pbl['alltext'] = texts
 
 vectorizer = CountVectorizer()
 vectorizer.fit_transform(pbl['alltext'])
@@ -82,24 +72,22 @@ Y = dataset[:,[5,7]]#dataset[:,[5,7]]
 Y = Y.astype('float')
 X = np.asarray(X).astype(np.float32)
 Y = np.asarray(Y).astype(np.float32)
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.33, shuffle=True)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, shuffle=True)
 
 # evaluate model
 model = build_model()
-
 model.summary()
 
-# Параметр patience определяет количество эпох, проверяемых на улучшение
-early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+# delete large useless odjects
+del list_space, X, Y, vector_title, dataset
 
-class PrintDot(keras.callbacks.Callback):
-  def on_epoch_end(self, epoch, logs):
-    if epoch % 100 == 0: print('')
-    print('.', end='')
+
+# Параметр patience определяет количество эпох, проверяемых на улучшение
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=15)
 
 EPOCHS = 1000
 
-history = model.fit(X_train, y_train, epochs=EPOCHS,validation_split = 0.2, verbose=0, callbacks=[early_stop, PrintDot()])
+history = model.fit(X_train, y_train, epochs=EPOCHS,validation_split = 0.2, verbose=1, callbacks=[early_stop])
 def plot_history(history):
   hist = pd.DataFrame(history.history)
   hist['epoch'] = history.epoch
@@ -136,13 +124,13 @@ test_predictions = model.predict(X_test).flatten()
 pred1 = test_predictions[0::2]
 pred2 = test_predictions[1::2]
 plt.scatter(y_test[:,0], pred1)
-plt.show()
-plt.scatter(y_test[:,1], pred2)
-#test_predictions = model.predict(X_train).flatten()
-#
-#plt.scatter(y_train, test_predictions)
 plt.xlabel('True Values [MPG]')
 plt.ylabel('Predictions [MPG]')
+plt.show()
+plt.scatter(y_test[:,1], pred2)
+plt.xlabel('True Values [MPG]')
+plt.ylabel('Predictions [MPG]')
+plt.show()
 #plt.axis('equal')
 #plt.axis('square')
 #plt.xlim([0,plt.xlim()[1]])
@@ -159,9 +147,7 @@ title = [title]
 vectorizer.vocabulary_ = title_voc
 vector = vectorizer.transform(title)
 vector = vector.toarray()
-
-test = [[vector]]
-test = np.asarray(test).astype(np.float32)
+test = np.asarray(vector).astype(np.float32)
 test_predictions = model.predict(test).flatten()
 print('Prediction is {} '.format(test_predictions))
 
